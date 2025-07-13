7ce5797ce47f38993038c141dd796f53fffa50f7
matrakugeri <matrakugeri@gmail.com>
2025-07-13 13:13:19 +0200

    added new margin dimensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
         responsiveGrid2: { max: "580px" },
         responsiveGrid3: { max: "400px" },
       },
+      margin: {
+        custom1: "2rem",
+        custom2: "5rem",
+        custom3: "8rem",
+      },
       gap: {
         flexGap: "2.5rem",
       },
@@ -52,9 +57,15 @@ module.exports = {
         headingColor: "#339af0",
         skillsColor: "#69db7c",
         PopColor: "#d8f5a2",
+        cyanColor: "#228be6",
       },
       boxShadow: {
         "3xl": "0 4px 10px rgba(211, 220, 225, 0.7)",
+        sampleShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+        hardShadow:
+          "rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px",
+        heavyShadow:
+          "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",
       },
     },
   },
